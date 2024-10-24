551159b3101d4075cef5bf3f8875a8db0db4277b
Aidan Johnston <aidanjhnston@gmail.com>
2024-10-24 00:57:22 -0400

    ever so slightly better cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,14 @@
-use clap::{Arg, ArgMatches, Command};
+use clap::{Arg, Command};
 
-pub fn get_cli_arg_matches() -> ArgMatches {
+pub fn get_cli_commnad() -> Command {
     return Command::new("mush")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("The mush scripting language.")
+        .author("Aidan Johnston <contact@aidanjohnston.ca>")
         .arg(
             Arg::new("file_path")
                 .index(1) // Specifies that this is a positional argument at position 1
                 .help("Path to the mush scrip to run.")
                 .required(false), // Makes it optional
-        )
-        .get_matches();
+        );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{
-    env,
-    io::{self, Error},
+    io::{self},
     path::Path,
 };
 
@@ -10,7 +9,7 @@ mod cli;
 mod engine;
 
 fn main() -> io::Result<()> {
-    let matches = cli::get_cli_arg_matches();
+    let matches = cli::get_cli_commnad().get_matches();
 
     if let Some(value) = matches.get_one::<String>("file_path") {
         let _ = run_from_file(Path::new(value))?;
